b005d28d3959b4b89eace0f76b7e8e385fb6a722
Kent Fredric <kentfredric@gmail.com>
2009-07-25 06:04:54 +1200

    Code reordered, documentation pairs with code, same ordering.
    This is made nice for us at release time when DZIL moves it all the way
    to the end.
    
    The primary reasons to keep POD after __END__ are for readability,
    as it suffers when you don't have a syntax highlighter or code folding
    available.
    
    However, when developing stuff, we have these facilities, and having the
    documentation closer to the code is more advantageous as it encourages
    keeping it up to date.
    
    PodPurler makes this a legitamate possibility

--- lib/Spark/Form.pm
+++ lib/Spark/Form.pm
@@ -2,6 +2,55 @@ package Spark::Form;
 
 # ABSTRACT: A simple yet powerful forms validation system that promotes reuse.
 
+=head1 SYNOPSIS
+
+ use Spark::Form;
+ use CGI; #Because it makes for a quick and oversimplistic example
+ use Third::Party::Field;
+ $form = Spark::Form->new(plugin_ns => 'MyApp::Field');
+ # Add a couple of inbuilt modules
+ $form->add('email','email',confirm_field => 'email-confirm')
+      ->add('email','email-confirm')
+      ->add('password','password',regex => qr/^\S{6,}$/),
+      #This one will be autoloaded from MyApp::Field::Username
+      ->add('username','username')
+      # And this shows how you can use a third party field of any class name
+      ->add(Third::Party::Field->new(name => 'blah'));
+ #Pass in a hashref of params to populate the virtual form with data
+ $form->data(CGI->new->params);
+ #And do the actual validation
+ if ($form->validate) {
+     print "You are now registered";
+ } else {
+     print join "\n", $form->errors;
+ }
+
+and over in MyApp/Field/Username.pm...
+
+ package MyApp::Form::Field::Username;
+ use base Spark::Form::Field;
+
+ sub validate {
+   my ($self,$v) = @_;
+
+   if (length $v < 6 or length $v > 12) {
+     $self->error("Usernames must be 6-12 characters long");
+   } elsif ($v =~ /[^a-zA-Z0-9_-]/) {
+     $self->error("Usernames may contain only a-z,A-Z,0-9, _ and -");
+   } else {
+     $self->error(undef);
+   }
+   $self->valid(!!$self->error());
+ }
+
+=cut
+
+=head1 DEPENDENCIES
+
+Moose. I've dropped using Any::Moose. If you need the performance increase, perhaps it's time to start thinking about shifting off CGI.
+
+=cut
+
 use Moose;
 use MooseX::AttributeHelpers;
 use List::MoreUtils 'all';
@@ -19,6 +68,29 @@ has _fields_a => (
     },
 );
 
+=head1 ATTRIBUTES
+
+=cut
+
+has plugin_ns => (
+    isa      => 'Str',
+    is       => 'ro',
+    required => 0,
+);
+
+has valid => (
+    isa      => 'Bool',
+    is       => 'rw',
+    required => 0,
+    default  => 0,
+);
+
+=head1 PRIVATE ATTRIBUTES
+
+No user servicable parts.
+
+=cut
+
 has _fields_h => (
     metaclass => 'Collection::Hash',
     isa       => 'HashRef',
@@ -33,12 +105,6 @@ has _fields_h => (
     },
 );
 
-has plugin_ns => (
-    isa      => 'Str',
-    is       => 'ro',
-    required => 0,
-);
-
 has _errors => (
     metaclass => 'Collection::Array',
     isa       => 'ArrayRef',
@@ -51,19 +117,149 @@ has _errors => (
         clear    => '_clear_errors',
     },);
 
-has valid => (
-    isa      => 'Bool',
-    is       => 'rw',
-    required => 0,
-    default  => 0,
-);
-
 has '_printer' => (
     isa      => 'Maybe[Str]',
     required => 0,
     is       => 'ro',
     init_arg => 'printer',
 );
+
+=head1 METHODS
+
+=cut
+
+=head2 import (%options)
+
+=begin author_note
+
+Really? this works? what makes it work?  FIXME
+
+=end
+
+Allows you to set some options for the forms class.
+
+=over 4
+
+=item class => String
+
+Optional, gives the basename for searching for form plugins.
+
+Given 'MyApp', it will try to load form plugins from MyApp::Form::Field::*
+
+=item source => String
+
+Optional, names a plugin to try and extract form data from.
+
+If unspecified, you will need to call $form->data(\%data);
+
+=back
+
+=cut
+
+=head2 add ($thing,@rest)
+
+If $thing is a string, attempts to instantiate a plugin of that type and add it
+to the form. Requires the second argument to be a string name for the field to identify it in the form. Rest will become %kwargs
+If it is an arrayref, it loops over the contents (Useful for custom fields, will probably result in bugs for string field names).@rest will be passed in each iteration.
+If it looks sufficiently like a field (implements Spark::Form::Field),
+then it will add it to the list of fields. @rest will just become %kwargs
+
+Uses 'field name' to locate it from the data passed in.
+
+This is a B<streaming interface>, it returns the form itself.
+
+=cut
+
+sub add {
+    my $self = shift;
+    my $item = shift;
+
+    #Dispatch to the appropriate handler sub
+
+    #1. Regular String. Should have a name and any optional args
+    return do { die unless scalar @_; $self->_add_by_type($item, @_); $self }
+      unless ref $item;
+
+    #2. Array - loop. This will spectacularly fall over if you are using string-based creation as there's no way to pass multiple names yet
+    return do { $self->add($_, @_) for @$item; $self }
+      if ref $item eq 'ARRAY';
+
+    #3. Custom field. Just takes any optional args
+    return do { $self->_add_custom_field($item, @_); $self }
+      if $self->_valid_custom_field($item);
+
+    #Unknown thing
+    die("Spark::Form: Don't know what to do with a " . ref $item);
+}
+
+=head2 get (Str)
+
+Returns the form field of that name
+
+=cut
+
+sub get {
+    my ($self, $key) = @_;
+
+    $self->_get_fields_h($key);
+}
+
+=head2 validate
+
+Validates the form. Sets C<valid> and then also returns the value.
+
+=cut
+
+sub validate {
+    my ($self) = @_;
+
+    #Clear out
+    $self->valid(1);
+    $self->_clear_errors();
+    if (all { $_->meta->does_role('Spark::Form::Field::Role::Validateable') } $self->fields_a) {
+        foreach my $field ($self->fields_a) {
+            $field->validate;
+            unless ($field->valid) {
+                $self->_error($_) foreach $field->errors;
+            }
+        }
+        $self->valid;
+    } else {
+        die("Not all fields in this form are validateable.");
+    }
+}
+
+=head2 data
+
+Allows you to pass in a hashref of data to populate the fields with before validation. Useful if you don't use a plugin to automatically populate the data.
+
+This is a B<streaming interface>, it returns the form itself.
+
+=cut
+
+sub data {
+    my ($self, $fields) = @_;
+    while (my ($k, $v) = each %$fields) {
+        if ($self->_has_fields_h($k)) {
+            $self->_get_fields_h($k)->value($v);
+        }
+    }
+
+    $self;
+}
+
+=head1 PRIVATE METHODS
+
+No user servicable parts
+
+=cut
+
+=head2 BUILD
+
+Moose constructor. Test::Pod::Coverage made me do it.
+Adds C<class> to the search path for field modules.
+
+=cut
 
 sub BUILD {
     my ($self) = @_;
@@ -97,71 +293,6 @@ sub BUILD {
     }
 }
 
-sub _error {
-    my ($self, $error) = @_;
-
-    $self->valid(0);
-    $self->_add_error($error);
-}
-
-sub add {
-    my $self = shift;
-    my $item = shift;
-
-    #Dispatch to the appropriate handler sub
-
-    #1. Regular String. Should have a name and any optional args
-    return do { die unless scalar @_; $self->_add_by_type($item, @_); $self }
-      unless ref $item;
-
-    #2. Array - loop. This will spectacularly fall over if you are using string-based creation as there's no way to pass multiple names yet
-    return do { $self->add($_, @_) for @$item; $self }
-      if ref $item eq 'ARRAY';
-
-    #3. Custom field. Just takes any optional args
-    return do { $self->_add_custom_field($item, @_); $self }
-      if $self->_valid_custom_field($item);
-
-    #Unknown thing
-    die("Spark::Form: Don't know what to do with a " . ref $item);
-}
-
-sub get {
-    my ($self, $key) = @_;
-
-    $self->_get_fields_h($key);
-}
-
-sub validate {
-    my ($self) = @_;
-
-    #Clear out
-    $self->valid(1);
-    $self->_clear_errors();
-    if (all { $_->meta->does_role('Spark::Form::Field::Role::Validateable') } $self->fields_a) {
-        foreach my $field ($self->fields_a) {
-            $field->validate;
-            unless ($field->valid) {
-                $self->_error($_) foreach $field->errors;
-            }
-        }
-        $self->valid;
-    } else {
-        die("Not all fields in this form are validateable.");
-    }
-}
-
-sub data {
-    my ($self, $fields) = @_;
-    while (my ($k, $v) = each %$fields) {
-        if ($self->_has_fields_h($k)) {
-            $self->_get_fields_h($k)->value($v);
-        }
-    }
-
-    $self;
-}
-
 sub _valid_custom_field {
     my ($self, $thing) = @_;
     eval {
@@ -184,6 +315,13 @@ sub _add_by_type {
 
     #Create and add it
     $self->_add($self->_create_type($type, $name, %opts), $name);
+}
+
+sub _error {
+    my ($self, $error) = @_;
+
+    $self->valid(0);
+    $self->_add_error($error);
 }
 
 sub _add {
@@ -245,103 +383,9 @@ sub _create_type {
 1;
 __END__
 
-=head1 SYNOPSIS
 
- use Spark::Form;
- use CGI; #Because it makes for a quick and oversimplistic example
- use Third::Party::Field;
- $form = Spark::Form->new(plugin_ns => 'MyApp::Field');
- # Add a couple of inbuilt modules
- $form->add('email','email',confirm_field => 'email-confirm')
-      ->add('email','email-confirm')
-      ->add('password','password',regex => qr/^\S{6,}$/),
-      #This one will be autoloaded from MyApp::Field::Username
-      ->add('username','username')
-      # And this shows how you can use a third party field of any class name
-      ->add(Third::Party::Field->new(name => 'blah'));
- #Pass in a hashref of params to populate the virtual form with data
- $form->data(CGI->new->params);
- #And do the actual validation
- if ($form->validate) {
-     print "You are now registered";
- } else {
-     print join "\n", $form->errors;
- }
 
-and over in MyApp/Field/Username.pm...
 
- package MyApp::Form::Field::Username;
- use base Spark::Form::Field;
-
- sub validate {
-   my ($self,$v) = @_;
-
-   if (length $v < 6 or length $v > 12) {
-     $self->error("Usernames must be 6-12 characters long");
-   } elsif ($v =~ /[^a-zA-Z0-9_-]/) {
-     $self->error("Usernames may contain only a-z,A-Z,0-9, _ and -");
-   } else {
-     $self->error(undef);
-   }
-   $self->valid(!!$self->error());
- }
-
-=head1 DEPENDENCIES
-
-Moose. I've dropped using Any::Moose. If you need the performance increase, perhaps it's time to start thinking about shifting off CGI.
-
-=head1 METHODS
-
-=head2 import (%options)
-
-Allows you to set some options for the forms class.
-
-=over 4
-
-=item class => String
-
-Optional, gives the basename for searching for form plugins.
-
-Given 'MyApp', it will try to load form plugins from MyApp::Form::Field::*
-
-=item source => String
-
-Optional, names a plugin to try and extract form data from.
-
-If unspecified, you will need to call $form->data(\%data);
-
-=back
-
-=head2 add ($thing,@rest)
-
-If $thing is a string, attempts to instantiate a plugin of that type and add it
-to the form. Requires the second argument to be a string name for the field to identify it in the form. Rest will become %kwargs
-If it is an arrayref, it loops over the contents (Useful for custom fields, will probably result in bugs for string field names).@rest will be passed in each iteration.
-If it looks sufficiently like a field (implements Spark::Form::Field),
-then it will add it to the list of fields. @rest will just become %kwargs
-
-Uses 'field name' to locate it from the data passed in.
-
-This is a B<streaming interface>, it returns the form itself.
-
-=head2 validate
-
-Validates the form. Sets C<valid> and then also returns the value.
-
-=head2 data
-
-Allows you to pass in a hashref of data to populate the fields with before validation. Useful if you don't use a plugin to automatically populate the data.
-
-This is a B<streaming interface>, it returns the form itself.
-
-=head2 BUILD
-
-Moose constructor. Test::Pod::Coverage made me do it.
-Adds C<class> to the search path for field modules.
-
-=head2 get (Str)
-
-Returns the form field of that name
 
 =head1 Docs?
 
